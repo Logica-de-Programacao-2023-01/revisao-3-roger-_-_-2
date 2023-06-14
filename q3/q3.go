17b320f191e0a7c9dea8a5b19ff21f42ac001be0
Moeeru <126564425+Moeeru@users.noreply.github.com>
2023-06-14 18:39:35 -0300

    q3.go

--- q3/q3.go
+++ q3/q3.go
@@ -5,5 +5,20 @@ package q3
 //A cada vez, você pode subir 1 ou 2 degraus. De quantas maneiras distintas você pode subir até o topo?
 
 func ClimbStairs(n int) int {
-	return 0
+	pos := 0
+	menos := 1
+	if n%2 == 0 {
+		pos++
+		for i := 1; i < n; i++ {
+			pos += fatorial(n-menos) / (fatorial(n-i-menos) * fatorial(i))
+			menos++
+		}
+	} else {
+		pos++
+		for i := 1; i <= (n / 2); i++ {
+			pos += fatorial(n-menos) / (fatorial(n-i-menos) * fatorial(i))
+			menos++
+		}
+	}
+	return pos
 }
