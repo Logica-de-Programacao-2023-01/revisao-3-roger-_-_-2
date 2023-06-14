f4b1a72f0c4cbfc67bfcf98fcf40ad79ffacd14e
Moeeru <126564425+Moeeru@users.noreply.github.com>
2023-06-14 17:44:12 -0300

    q2.go

--- q2/q2.go
+++ q2/q2.go
@@ -5,5 +5,17 @@ package q2
 //Se não houver um prefixo comum, retorne uma string vazia "".
 
 func LongestCommonPrefix(strs []string) string {
-	return ""
+	if len(strs) == 0 {
+		return ""
+	}
+	prefix := strs[0]
+	for i := 0; i < len(strs); i++ {
+		for !strings.HasPrefix(strs[i], prefix) {
+			prefix = prefix[:len(prefix)-1]
+			if prefix == "" {
+				return ""
+			}
+		}
+	}
+	return prefix
 }
